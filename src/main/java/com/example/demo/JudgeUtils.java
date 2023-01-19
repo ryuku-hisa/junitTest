14901aa4f08a6f4833cb626f8b681b47fe902b1a
r-hisasue <r-hisasue@localhost>
2023-01-19 16:03:32 +0900

    プレイ年齢と課金年齢で条件分岐を行なった

--- src/main/java/com/example/demo/JudgeUtils.java
+++ src/main/java/com/example/demo/JudgeUtils.java
@@ -6,7 +6,9 @@ import java.util.Date;
 public class JudgeUtils extends MiraiPrjApplication {
 
 	// 定数
-	public static final int AGE = 17;
+	public static final int PLAY_AGE = 17;
+	public static final int BILL_AGE = 18;
+	
 
 	/**
 	 * 課金対象か判定するメソッド<br>
@@ -26,7 +28,7 @@ public class JudgeUtils extends MiraiPrjApplication {
 		// 現在の年月日のみをセット
 		compareCal.set(curYear, curMonth, curDate);
 		// 年からプレイ可能年齢を引く
-		compareCal.add(Calendar.YEAR, -AGE);
+		compareCal.add(Calendar.YEAR, -BILL_AGE);
 
 		// 生年月日を設定
 		Calendar birthCal = Calendar.getInstance();
@@ -58,7 +60,7 @@ public class JudgeUtils extends MiraiPrjApplication {
 		// 現在の年月日のみをセット
 		compareCal.set(curYear, curMonth, curDate);
 		// 年からプレイ可能年齢を引く
-		compareCal.add(Calendar.YEAR, -AGE);
+		compareCal.add(Calendar.YEAR, -PLAY_AGE);
 
 		// 生年月日を設定
 		Calendar birthCal = Calendar.getInstance();
